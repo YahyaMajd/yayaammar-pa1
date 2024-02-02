bc5a7d296ca657dd32504c292caea494717a2965
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2024-02-02 00:34:34 -0800

    added skeleton for main

--- src/receiver.c
+++ src/receiver.c
@@ -16,3 +16,19 @@ void rrecv(unsigned short int myUDPport,
             unsigned long long int writeRate) {
 
 }
+
+int main(int argc, char** argv) {
+    // This is a skeleton of a main function.
+    // You should implement this function more completely
+    // so that one can invoke the file transfer from the
+    // command line.
+
+    unsigned short int udpPort;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
+        exit(1);
+    }
+
+    udpPort = (unsigned short int) atoi(argv[1]);
+}
--- src/sender.c
+++ src/sender.c
@@ -18,3 +18,23 @@ void rsend(char* hostname,
 {
 
 }
+
+int main(int argc, char** argv) {
+    // This is a skeleton of a main function.
+    // You should implement this function more completely
+    // so that one can invoke the file transfer from the
+    // command line.
+    int hostUDPport;
+    unsigned long long int bytesToTransfer;
+    char* hostname = NULL;
+
+    if (argc != 5) {
+        fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
+        exit(1);
+    }
+    hostUDPport = (unsigned short int) atoi(argv[2]);
+    hostname = argv[1];
+    bytesToTransfer = atoll(argv[4]);
+
+    return (EXIT_SUCCESS);
+}
