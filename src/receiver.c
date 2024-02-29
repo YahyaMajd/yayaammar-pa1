d32feb3827e4e91a3184afd28c9753b2714054af
YahyaMajd <112823817+YahyaMajd@users.noreply.github.com>
2024-02-29 13:26:47 -0800

    new start

--- src/receiver.c
+++ src/receiver.c
@@ -3,102 +3,97 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#include <pthread.h>
 #include <errno.h>
-#include <time.h>
 
-#define BUFFER_SIZE 1024  // Adjust based on expected data packet size
 
-struct ack_packet {
-    unsigned int seq_num; // Sequence number of the acknowledged packet
-};
+#include <signal.h>
+
+FILE *file; // Make sure file is accessible in the signal handler context
+
+void sigint_handler(int sig) {
+    printf("Caught signal %d, closing file and exiting.\n", sig);
+    if (file != NULL) {
+        fclose(file); // Close the file on SIGINT
+        file = NULL; // Prevent use-after-free
+    }
+    exit(0); // Exit gracefully
+}
 
 void rrecv(unsigned short int myUDPport, char* destinationFile, unsigned long long int writeRate) {
     int sockfd;
-    struct sockaddr_in myAddr, senderAddr;
-    char buffer[BUFFER_SIZE];
+    struct sockaddr_in my_addr;
+    char buffer[1024];
     FILE *file;
-    unsigned long long int bytesWritten = 0;
-    unsigned int expectedSeqNum = 0; // Expected sequence number initialization
-    socklen_t senderAddrLen = sizeof(senderAddr);
 
-    // Create UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Bind socket to port
-    memset(&myAddr, 0, sizeof(myAddr));
-    myAddr.sin_family = AF_INET;
-    myAddr.sin_addr.s_addr = INADDR_ANY;
-    myAddr.sin_port = htons(myUDPport);
-    if (bind(sockfd, (struct sockaddr *) &myAddr, sizeof(myAddr)) < 0) {
-        perror("Bind failed");
-        close(sockfd);
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    my_addr.sin_port = htons(myUDPport);
+
+    if (bind(sockfd, (const struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+        perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Open file for writing
     file = fopen(destinationFile, "wb");
-    if (!file) {
-        perror("File opening failed");
-        close(sockfd);
+    if (file == NULL) {
+        perror("Failed to open file");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        ssize_t recvLen = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&senderAddr, &senderAddrLen);
-        if (recvLen < 0) {
-            perror("Receive failed");
-            break;
+    struct sockaddr_in sender_addr;
+    socklen_t addr_len = sizeof(sender_addr); // Correct type for len
+    int n;
+
+    while ((n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &sender_addr, &addr_len)) > 0) {
+        printf("Received %d bytes\n", n); // Print the number of received bytes
+
+        // Optionally print the sender's IP address
+        char senderIP[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &sender_addr.sin_addr, senderIP, sizeof(senderIP));
+        printf("From %s\n", senderIP);
+
+        // If the data is expected to be text, print the received text (ensure it's null-terminated)
+        buffer[n] = '\0'; // Make sure there's no buffer overflow here
+        printf("Received packet contains: \"%s\"\n", buffer);
+
+        if (fwrite(buffer, 1, n, file) != n) {
+            perror("Failed to write to file");
+            break; // Handle write error
         }
-
-        // Assuming the sequence number is the first 4 bytes of the packet
-        if (recvLen >= sizeof(unsigned int)) {
-            unsigned int seqNum;
-            memcpy(&seqNum, buffer, sizeof(unsigned int));
-            seqNum = ntohl(seqNum); // Convert from network byte order to host byte order
-
-            struct ack_packet ack;
-            ack.seq_num = htonl(seqNum); // Always send ACK for received packet
-
-            // Only write data if it's the expected sequence number
-            if (seqNum == expectedSeqNum) {
-                fwrite(buffer + sizeof(unsigned int), 1, recvLen - sizeof(unsigned int), file);
-                bytesWritten += recvLen - sizeof(unsigned int);
-                expectedSeqNum++; // Increment expected sequence number for the next packet
-            } else {
-                // If not the expected sequence number, do not write data
-                // This implicitly requests retransmission of lost packets
-            }
-
-            // Send ACK for the received packet
-            if (sendto(sockfd, &ack, sizeof(ack), 0, (struct sockaddr *)&senderAddr, senderAddrLen) < 0) {
-                perror("ACK sendto failed");
-            }
-        }
+        fflush(file); 
     }
 
-    fclose(file); // Close the file
-    close(sockfd); // Close the socket
+
+    fclose(file);
+    close(sockfd);
 }
 
+
 int main(int argc, char** argv) {
-    // This is a skeleton of a main function.
-    // You should implement this function more completely
-    // so that one can invoke the file transfer from the
-    // command line.
-
-    unsigned short int udpPort;
-
+    signal(SIGINT, sigint_handler);
     if (argc != 3) {
-        fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "usage: %s UDP_port filename_to_write\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    udpPort = (unsigned short int) atoi(argv[1]);
+    unsigned short int myUDPport = (unsigned short int)atoi(argv[1]);
+    char* destinationFile = argv[2];
+    unsigned long long int writeRate = 0; // Placeholder, adjust as needed
+
+    rrecv(myUDPport, destinationFile, writeRate);
+
+    return EXIT_SUCCESS;
 }
--- src/sender.c
+++ src/sender.c
@@ -3,189 +3,91 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <sys/socket.h>
+
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
+
+#include <pthread.h>
 #include <errno.h>
-#include <time.h>
 
-#define BUFFER_SIZE 1024
-#define HEADER_SIZE 8
-#define DATA_SIZE (BUFFER_SIZE - HEADER_SIZE)
-#define ACK_TIMEOUT 2  // Timeout in seconds for ACKs
-#define INITIAL_PACKETS_SIZE 10  // Initial size of the packets array
-
-struct packet {
-    unsigned int seq_num;
-    char data[DATA_SIZE];
-    int acked;  // 1 if ACK received, 0 otherwise
-    time_t send_time;  // Time when the packet was last sent
-};
-
-struct ack_packet {
-    unsigned int seq_num;
-};
-
-struct packet *packets = NULL; // Dynamic array of packets
-int num_packets = 0;  // Number of packets currently being tracked
-int allocated_packets = 0; // Allocated size of the packets array
-
-void resize_packets_array() {
-    if (num_packets >= allocated_packets) {
-        int new_size = allocated_packets == 0 ? INITIAL_PACKETS_SIZE : allocated_packets * 2;
-        struct packet *new_array = realloc(packets, new_size * sizeof(struct packet));
-        if (!new_array) {
-            perror("Failed to resize packets array");
-            exit(EXIT_FAILURE);
-        }
-        packets = new_array;
-        allocated_packets = new_size;
-    }
-}
-
-void resend_packets(int sockfd, struct sockaddr_in *receiverAddr) {
-    time_t current_time = time(NULL);
-
-    for (int i = 0; i < num_packets; i++) {
-        if (!packets[i].acked && difftime(current_time, packets[i].send_time) > ACK_TIMEOUT) {
-            char buffer[BUFFER_SIZE];
-            *((unsigned int *)buffer) = htonl(packets[i].seq_num);
-            memcpy(buffer + HEADER_SIZE, packets[i].data, DATA_SIZE);
-
-            if (sendto(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)receiverAddr, sizeof(*receiverAddr)) < 0) {
-                perror("Sendto failed during retransmission");
-                continue;
-            }
-
-            packets[i].send_time = current_time;
-            printf("Packet %u retransmitted.\n", packets[i].seq_num);
-        }
-    }
-}
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h> // for opening file
 
 void rsend(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
     int sockfd;
-    struct sockaddr_in receiverAddr;
+    struct sockaddr_in receiver_addr;
+    char buffer[1024];
     FILE *file;
-    unsigned long long int bytesSent = 0;
-    unsigned int seqNum = 0;
-    char buffer[BUFFER_SIZE];
-    struct timeval tv;
 
-    // Initialize dynamic array for packets
-    resize_packets_array(); // Ensure we have an initial allocation
-
-    // Create UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
+    // Create socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Set socket timeout for receiving ACKs
-    tv.tv_sec = ACK_TIMEOUT;
-    tv.tv_usec = 0;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv)) < 0) {
-        perror("Error setting socket timeout");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
+    memset(&receiver_addr, 0, sizeof(receiver_addr));
 
-    // Setup receiver address
-    memset(&receiverAddr, 0, sizeof(receiverAddr));
-    receiverAddr.sin_family = AF_INET;
-    receiverAddr.sin_port = htons(hostUDPport);
-    if (inet_pton(AF_INET, hostname, &receiverAddr.sin_addr) <= 0) {
+    // Filling server information
+    receiver_addr.sin_family = AF_INET;
+    receiver_addr.sin_port = htons(hostUDPport);
+    receiver_addr.sin_addr.s_addr = inet_addr(hostname); // Convert IPv4 addresses from text to binary form
+    if (inet_pton(AF_INET, hostname, &receiver_addr.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
-
-    // Open file
+    // Open the file
     file = fopen(filename, "rb");
-    if (!file) {
-        perror("File opening failed");
-        close(sockfd);
+    if (file == NULL) {
+        perror("Failed to open file");
         exit(EXIT_FAILURE);
     }
+    printf("File opened successfully.\n");
 
-    while (bytesSent < bytesToTransfer) {
-        ssize_t readBytes = fread(buffer + HEADER_SIZE, 1, DATA_SIZE, file);
-        if (readBytes > 0) {
-            ssize_t packetSize = HEADER_SIZE + readBytes;
-            *((unsigned int*)buffer) = htonl(seqNum); // Sequence number in network byte order
-
-            // Call resend_packets to retransmit any packets that have not been ACKed in time
-            resend_packets(sockfd, &receiverAddr);
-
-            // Transmit the packet
-            if (sendto(sockfd, buffer, packetSize, 0, (struct sockaddr*)&receiverAddr, sizeof(receiverAddr)) < 0) {
-                perror("Sendto failed");
-                break;
-            }
-
-            // Prepare for tracking the packet
-            resize_packets_array();
-            packets[num_packets].seq_num = seqNum;
-            memcpy(packets[num_packets].data, buffer + HEADER_SIZE, readBytes);
-            packets[num_packets].acked = 0; // Initially not acknowledged
-            packets[num_packets].send_time = time(NULL); // Record the send time
-            num_packets++; // Increment the packet counter
-
-            struct ack_packet ack;
-            socklen_t fromlen = sizeof(receiverAddr);
-
-            // Attempt to receive ACK
-            if (recvfrom(sockfd, &ack, sizeof(ack), 0, (struct sockaddr*)&receiverAddr, &fromlen) >= 0) {
-                ack.seq_num = ntohl(ack.seq_num); // Convert to host byte order
-                for (int i = 0; i < num_packets; i++) {
-                    if (packets[i].seq_num == ack.seq_num) {
-                        packets[i].acked = 1; // Mark as acknowledged
-                        printf("ACK received for packet %u\n", ack.seq_num);
-                        break;
-                    }
-                }
-            } else {
-                if (errno != EWOULDBLOCK) {
-                    perror("Recvfrom failed");
-                    break;
-                }
-            }
-
-            bytesSent += readBytes;
-            seqNum++;
-        } else {
-            if (ferror(file)) {
-                perror("Error reading from file");
-                break;
-            }
+    // Read and send the file in chunks
+    unsigned long long int bytesSent = 0;
+    while (bytesSent < bytesToTransfer && !feof(file)) {
+        size_t toRead = sizeof(buffer);
+        if (bytesToTransfer - bytesSent < toRead) {
+            toRead = bytesToTransfer - bytesSent;
         }
+        size_t read = fread(buffer, 1, toRead, file);
+        if (sendto(sockfd, buffer, read, 0, (const struct sockaddr *) &receiver_addr, sizeof(receiver_addr)) < 0) {
+            perror("sendto failed");
+            break;
+        }
+        bytesSent += read;
+        printf("Sent %zu bytes, total sent: %llu bytes.\n", read, bytesSent);
     }
 
-    // Cleanup
+    if (bytesSent >= bytesToTransfer) {
+        printf("Specified amount of data sent successfully.\n");
+    } else {
+        printf("Reached end of file before sending specified amount of data.\n");
+    }
+
     fclose(file);
     close(sockfd);
-    free(packets); // Free the dynamically allocated packets array at the end
+    printf("File and socket closed, exiting.\n");
 }
 
 
 
 int main(int argc, char** argv) {
-    // This is a skeleton of a main function.
-    // You should implement this function more completely
-    // so that one can invoke the file transfer from the
-    // command line.
-    int hostUDPport;
-    unsigned long long int bytesToTransfer;
-    char* hostname = NULL;
-
     if (argc != 5) {
-        fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    hostUDPport = (unsigned short int) atoi(argv[2]);
-    hostname = argv[1];
-    bytesToTransfer = atoll(argv[4]);
 
-    return (EXIT_SUCCESS);
+    char* hostname = argv[1];
+    unsigned short int hostUDPport = (unsigned short int)atoi(argv[2]);
+    char* filename = argv[3];
+    unsigned long long int bytesToTransfer = atoll(argv[4]);
+
+    rsend(hostname, hostUDPport, filename, bytesToTransfer);
+
+    return EXIT_SUCCESS;
 }
